1d5abfc2e9b3b132ee4d8d0cd4c992eae1d99c2d
tomwang1013 <174604588@qq.com>
2018-12-10 09:29:05 +0800

    comment images

--- client/pages/comment/comment.js
+++ client/pages/comment/comment.js
@@ -66,52 +66,14 @@ Page({
     })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  // 预览图片
+  previewImg(event) {
+    let target = event.currentTarget
+    let src = target.dataset.src
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    wx.previewImage({
+      current: src,
+      urls: this.data.images
+    })
   }
 })
